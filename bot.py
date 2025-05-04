04840ad6a00b2c6ef8019a1f987e16c32f2f339f
ButterFurry <joshhalseyqld@gmail.com>
2025-05-04 16:43:17 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
+DCTOKEN = os.environ.get('DCTOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True  # Add this
@@ -57,4 +59,4 @@ async def on_voice_state_update(member, before, after):
     finally:
         await vc_client.disconnect()
 
-bot.run("")
+bot.run(DCTOKEN)
